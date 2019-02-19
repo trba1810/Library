1a50ecdf9b949963ed4c90f5f2c04f6113e7bb51
Tomislav Trbic <TRBA8L@live.com>
2019-02-19 18:45:35 +0100

    Add project files.

---
+++ DAL/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Connection
+    {
+        public static string kon = ConfigurationManager.ConnectionStrings["Default"].ToString();
+    }
+}
---
+++ Entity/Knjiga.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Knjiga
+    {
+        public int ID_Knjige;
+        public string Naziv_Knjige;
+        public string Zanr;
+        public int ID_Zanr;
+
+        public Knjiga()
+        {
+
+        }
+
+        public Knjiga(int id, string naziv, string zanr, int id_z)
+        {
+            this.ID_Knjige = id;
+            this.Naziv_Knjige = naziv;
+            this.Zanr = zanr;
+            this.ID_Zanr = id_z;
+        }
+
+        public Knjiga(int id)
+        {
+            this.ID_Knjige = id;
+        }
+    }
+}
